7bdc68931982e214bb8783ef46a4f323840a2497
Ivan Dozorov <oozo1101@yandex.ru>
2022-05-14 23:09:17 +0300

    refactor: delete useLocation in footer, add route to footer and fix style header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {useLocation} from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const Footer = () => {
-    const location = useLocation();
 
     return (
         <>
-            {
-                location.pathname === '/sign-up' || location.pathname === '/sign-in' ? (<></>) :
-                    (<footer className="footer">
-                        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
-                    </footer>)
-            }
+            <Route exact path='/'>
+                <footer className="footer">
+                    <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+                </footer>
+            </Route>
         </>
     );
 };
